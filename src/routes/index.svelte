a1362ef648bb3d949c49e819ee78276776c11560
Youtube-YT <mingweiop@gmail.com>
2022-01-23 03:55:30 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    let uid = "";
+    let name = "";
+    let gender = "";
+    let height = "";
+    let weight = "";
+    let hair_color = "";
+    let eye_color = "";
+    let birth_year = "";
+    function getInfo() {
+        
+    }
+</script>
